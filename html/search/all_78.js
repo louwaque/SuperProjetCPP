6daaee5c2fae6ba4ded79135d4c4cb74ccd1680a
Travis CI <travis@travis-ci.org>
2017-05-22 11:05:26 +0000

    Deploy code docs to GitHub Pages Travis build: 6
    Commit: dc94b53f0160bd11a95f320afe31f0b616ef282a

--- html/search/all_78.js
+++ html/search/all_78.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['x',['x',['../class_point.html#a4450c59e5d0fda1ca664bf7f14a72566',1,'Point::x()'],['../ez-draw_09_09_8hpp.html#ab4ada4f5b7011a8a08fd77572ddabc1aacab1e15e82c5976bfb476ddfe145263c',1,'x():&#160;ez-draw++.hpp'],['../ez-draw_09_09_8hpp.html#ab4ada4f5b7011a8a08fd77572ddabc1aa58833a3110c570fb05130d40c365d1e4',1,'X():&#160;ez-draw++.hpp']]]
+  ['x',['x',['../class_point.html#a2aa897aa68c2f3e6df3a46738f27a0ac',1,'Point::x() const '],['../class_point.html#a4450c59e5d0fda1ca664bf7f14a72566',1,'Point::x()'],['../ez-draw_09_09_8hpp.html#ab4ada4f5b7011a8a08fd77572ddabc1aacab1e15e82c5976bfb476ddfe145263c',1,'x():&#160;ez-draw++.hpp'],['../ez-draw_09_09_8hpp.html#ab4ada4f5b7011a8a08fd77572ddabc1aa58833a3110c570fb05130d40c365d1e4',1,'X():&#160;ez-draw++.hpp']]]
 ];
--- html/search/functions_70.js
+++ html/search/functions_70.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['paint',['paint',['../classfinal.html#a3a610d4dfd7c4d2ad42d6b25e5ff47ab',1,'final::paint(EZWindow &amp;win, int x, int y) const '],['../classfinal.html#a3a610d4dfd7c4d2ad42d6b25e5ff47ab',1,'final::paint(EZWindow &amp;win, int x, int y) const ']]],
   ['paintsubimage',['paintSubimage',['../classfinal.html#aebe22646f553cd099aee45754deace05',1,'final']]],
+  ['point',['Point',['../class_point.html#a1c8a6881ed0f11828299033c5959d147',1,'Point::Point(int x=0, int y=0)'],['../class_point.html#a7485d0952c4bf845fc02bb1a75cbdcc1',1,'Point::Point(const Point &amp;src)']]],
   ['print',['print',['../classfinal.html#ab60ff6c7afa439bb21a9d320d3794341',1,'final']]]
 ];
