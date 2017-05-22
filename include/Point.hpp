3317146ccc9cf2c11e623eb732c447822dc28e44
L0L022 <L0L022@openmailbox.org>
2017-05-22 08:56:08 +0200

    ajout operator de Point

--- include/Point.hpp
+++ include/Point.hpp
@@ -1,6 +1,8 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
+#include <iostream>
+
 class Point {
 public:
   Point(int x = 0, int y = 0);
@@ -25,5 +27,13 @@ public:
 private:
   int m_x, m_y;
 };
+
+bool operator==(const Point &l, const Point &r);
+Point operator+(const Point &l, const Point &r);
+Point operator+(const Point &p, const int number);
+Point operator-(const Point &l, const Point &r);
+Point operator-(const Point &p, const int number);
+std::ostream &operator<<(std::ostream &os, const Point &p);
+bool operator<(const Point &l, const Point &r);
 
 #endif
--- src/Point.cpp
+++ src/Point.cpp
@@ -4,3 +4,31 @@ Point::Point(int x, int y)
   : m_x(x), m_y(y)
 {
 }
+
+bool operator==(const Point &l, const Point &r) {
+  return l.x() == r.x() and l.y() == r.y();
+}
+
+Point operator+(const Point &l, const Point &r) {
+  return Point(l.x()+r.x(), l.y()+r.y());
+}
+
+Point operator+(const Point &p, const int number) {
+  return Point(p.x()+number, p.y()+number);
+}
+
+Point operator-(const Point &l, const Point &r) {
+  return Point(l.x()-r.x(), l.y()-r.y());
+}
+
+Point operator-(const Point &p, const int number) {
+  return Point(p.x()-number, p.y()-number);
+}
+
+std::ostream &operator<<(std::ostream &os, const Point &p) {
+  return os << p.x() << " " << p.y();
+}
+
+bool operator<(const Point &l, const Point &r) {
+  return l.x() < r.x() and l.y() < r.y();
+}
