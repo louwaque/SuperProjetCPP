02a8d85757d3af2578117cea4486ce036bab1838
L0L022 <L0L022@openmailbox.org>
2017-05-29 10:20:44 +0200

    widget ajoute typedef

--- super-ez-lib/widget/include/LineEdit.hpp
+++ super-ez-lib/widget/include/LineEdit.hpp
@@ -14,7 +14,7 @@ public:
   inline std::string text() const { return m_text->text(); }
   void setText(const std::string &text);
 
-  inline boost::signals2::connection textChanged(const signal_t::slot_type &subscriber) { return m_textChanged.connect(subscriber); }
+  inline co_t textChanged(const slot_t &subscriber) { return m_textChanged.connect(subscriber); }
 
 protected:
   void meHandleEvent(const Event &event);
