464a7caece9388f47be1021e7cf6b5820e1de103
L0L022 <L0L022@openmailbox.org>
2017-06-02 16:51:58 +0200

    Améliore GraphicsText

--- super-ez-lib/graphics/src/GraphicsText.cpp
+++ super-ez-lib/graphics/src/GraphicsText.cpp
@@ -1,9 +1,11 @@
 #include "../include/GraphicsText.hpp"
 
 GraphicsText::GraphicsText(GraphicsItem *parent)
-: GraphicsItem(parent), m_text(""), m_font(), m_align(EZAlign::TL)
-{
-}
+: GraphicsItem(parent),
+  m_text(""),
+  m_font(),
+  m_align(EZAlign::TL)
+{}
 
 GraphicsText::GraphicsText(const std::string &text, GraphicsItem *parent)
 : GraphicsText(parent)
@@ -13,13 +15,14 @@ GraphicsText::GraphicsText(const std::string &text, GraphicsItem *parent)
 
 void GraphicsText::meDraw(Canvas *canvas)
 {
-  // if (canvas) {
-  //   canvas->setFont(m_font);
-  //   canvas->drawText(m_align, absolute(), m_text);
-  // }
+  if (canvas) {
+    canvas->setFont(m_font);
+    canvas->drawText(m_align, position(), m_text);
+  }
 }
 
 bool GraphicsText::meIsOver(const Point &absoluteP)
 {
+  //FIXME compliqué mais faisable; dépend de align
   return false;
 }
