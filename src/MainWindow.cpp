c085131a79f0e490549660777acbf00f3afc88df
L0L022 <L0L022@openmailbox.org>
2017-06-06 21:51:43 +0200

    Utilise les nouvelles méthodes

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -13,87 +13,106 @@
 
 MainWindow::MainWindow()
 : Window(400, 400, "SuperProjetCPP"), m_scene(), currentItem(nullptr)
-{/*
+{
   GraphicsPoint *point = new GraphicsPoint(&m_scene);
-  point->setAbsolute({50, 50});
+  point->position().set(5, 5);
   point->setColor(Color(Color::Blue));
+
   GraphicsText *text = new GraphicsText("Je suis super fort !!!", point);
-  text->setRelative({0, 10});
+  text->position().setRelative(0, 10);
   text->setColor(Color(Color::Green));
   text->setFont(Font::w12h24);
+
   GraphicsText *text1 = new GraphicsText("EZDraw c'est rigolo ^^", point);
-  text1->setRelative({10, 20});
+  text1->position().setRelative(10, 30);
   text1->setColor(Color(Color::Green));
 
-  GraphicsPoint *point2 = new GraphicsPoint(&m_scene);
-  point2->setAbsolute({100, 50});
-  point2->setColor(Color(Color::Yellow));
-  GraphicsText *text2 = new GraphicsText("Bonjour les gens !!!!", point2);
-  text2->setColor(Color(Color::Red));
+  {
+    GraphicsRectangle *rectangle = new GraphicsRectangle(&m_scene);
+    rectangle->position().set(50, 50);
+    rectangle->topLeft().setRelative(-20, -20);
+    rectangle->bottomRight().setRelative(20, 20);
+    rectangle->setHeight(100);
+    rectangle->setWidth(300);
 
-  GraphicsLine *line = new GraphicsLine(&m_scene);
-  line->setAbsolute({50, 100});
-  line->second()->setRelative({80, 20});
+    GraphicsPoint *rp1 = new GraphicsPoint(rectangle);
+    rp1->position().join(rectangle->position());
 
-  GraphicsTriangle *triangle = new GraphicsTriangle(&m_scene);
-  triangle->setFill(true);
-  triangle->setAbsolute({200, 20});
-  triangle->second()->setRelative(Point(-30, 30));
-  triangle->third()->setRelative(Point(30, 30));
-*/
-  GraphicsRectangle *rectangle = new GraphicsRectangle(&m_scene);
-  rectangle->position().set(50, 50);
-  rectangle->topLeft().setRelative(-20, -20);
-  rectangle->bottomRight().setRelative(20, 20);
+    GraphicsPoint *rp2 = new GraphicsPoint(rectangle);
+    rp2->position().join(rectangle->topLeft());
 
-  GraphicsPoint *rp1 = new GraphicsPoint(rectangle);
-  rp1->position().join(rectangle->position());
+    GraphicsPoint *rp3 = new GraphicsPoint(rectangle);
+    rp3->position().join(rectangle->bottomRight());
+  }
 
-  GraphicsPoint *rp2 = new GraphicsPoint(rectangle);
-  rp2->position().join(rectangle->topLeft());
+  {
+    GraphicsTriangle *triangle = new GraphicsTriangle(&m_scene);
+    triangle->position().set(200, 200);
+    triangle->first().setRelative(0, -10);
+    triangle->second().setRelative(-5, 10);
+    triangle->third().setRelative(5, 10);
 
-  GraphicsPoint *rp3 = new GraphicsPoint(rectangle);
-  rp3->position().join(rectangle->bottomRight());
+    GraphicsPoint *p1 = new GraphicsPoint(triangle);
+    p1->position().join(triangle->position());
 
-/*
-  GraphicsRectangle *r2 = new GraphicsRectangle(&m_scene);
-  r2->relative().set(50, 10);
-  r2->topLeft().set(-20, -20);
-  r2->bottomRight().set(20, 20);
-  r2->topLeft().join(rectangle->bottomRight());*/
-/*
+    GraphicsPoint *p2 = new GraphicsPoint(triangle);
+    p2->position().join(triangle->first());
+
+    GraphicsPoint *p3 = new GraphicsPoint(triangle);
+    p3->position().join(triangle->second());
+
+    GraphicsPoint *p4 = new GraphicsPoint(triangle);
+    p4->position().join(triangle->third());
+  }
+
   GraphicsPolygon *polygon = new GraphicsPolygon(&m_scene);
   polygon->setFill(true);
-  polygon->setAbsolute({200, 100});
+  polygon->position().set(200, 100);
   polygon->setNbPoints(10);
   polygon->setNbPoints(5);
 
-  GraphicsCircle *circle = new GraphicsCircle(&m_scene);
-  circle->setAbsolute({100, 300});
-  circle->radius()->setRelative({50, 50});
+  {
+    GraphicsCircle *circle = new GraphicsCircle(&m_scene);
+    circle->position().set(100, 300);
+    circle->radiusPt().setRelative(50, 50);
 
-  new GraphicsBlinkAnimation(Color(Color::Blue), circle);
+    GraphicsPoint *p1 = new GraphicsPoint(circle);
+    p1->position().join(circle->position());
 
-  GraphicsEllipse *ellipse = new GraphicsEllipse(&m_scene);
-  ellipse->setAbsolute({200, 300});
-  ellipse->radius()->setRelative({100, 50});
+    GraphicsPoint *p2 = new GraphicsPoint(circle);
+    p2->position().join(circle->radiusPt());
 
-  GraphicsSquare *square = new GraphicsSquare(&m_scene);
-  square->setThick(5);
-  square->setAbsolute({500, 100});
-  square->bottomRight().set(80, 50);
+    new GraphicsBlinkAnimation(Color(Color::Blue), circle);
+  }
 
-  GraphicsPoint *sp1 = new GraphicsPoint(&m_scene);
-  sp1->relative().join(square->relative());
+  {
+    GraphicsEllipse *ellipse = new GraphicsEllipse(&m_scene);
+    ellipse->position().set(200, 300);
+    ellipse->radius().setRelative(100, 50);
 
-  GraphicsPoint *sp2 = new GraphicsPoint(square);
-  sp2->relative().join(square->bottomRight());
+    GraphicsBlinkAnimation *blink = new GraphicsBlinkAnimation(ellipse);
+    blink->setBlinkFrequency(500);
+  }
 
-  GraphicsBlinkAnimation *blink = new GraphicsBlinkAnimation(square);
-  blink->setBlinkFrequency(500);
+  {
+    GraphicsSquare *square = new GraphicsSquare(&m_scene);
+    square->setThick(5);
+    square->position().set(500, 100);
+    square->topLeft().setRelative(-10, -10);
+    square->bottomRight().setRelative(10, 10);
+
+    GraphicsPoint *p1 = new GraphicsPoint(square);
+    p1->position().join(square->position());
+
+    GraphicsPoint *p2 = new GraphicsPoint(square);
+    p2->position().join(square->topLeft());
+
+    GraphicsPoint *p3 = new GraphicsPoint(square);
+    p3->position().join(square->bottomRight());
+  }
 
   LineEdit *lineEdit = new LineEdit(&m_scene);
-  lineEdit->setAbsolute({100, 500});*/
+  lineEdit->position().set(100, 500);
 }
 
 void MainWindow::drawEvent()
