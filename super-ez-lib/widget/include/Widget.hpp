a5ef89635d8d534a197325cee27208a039f5152e
L0L022 <L0L022@openmailbox.org>
2017-06-06 22:52:58 +0200

    Améliore Widget

--- super-ez-lib/widget/include/Widget.hpp
+++ super-ez-lib/widget/include/Widget.hpp
@@ -1,9 +1,10 @@
 #ifndef WIDGET_HPP
 #define WIDGET_HPP
 
+#include <cmath>
 #include <boost/signals2/signal.hpp>
 #include <boost/bind.hpp>
-#include <GraphicsItem.hpp>
+#include <GraphicsRectangle.hpp>
 
 class Widget : public GraphicsItem
 {
@@ -13,6 +14,30 @@ public:
   typedef boost::signals2::connection co_t;
 
   explicit Widget(GraphicsItem *parent = nullptr);
+
+  inline size_t width() const { return std::max(m_width, minimumWidth()); }
+
+  void setWidth(const size_t width);
+
+  inline size_t height() const { return std::max(m_height, minimumHeight()); }
+
+  void setHeight(const size_t height);
+
+  virtual size_t minimumWidth() const { return 0; }
+
+  virtual size_t minimumHeight() const { return 0; }
+
+  inline co_t widthChanged(const slot_t &subscriber) { return m_widthChanged.connect(subscriber); }
+  inline co_t heightChanged(const slot_t &subscriber) { return m_heightChanged.connect(subscriber); }
+
+private:
+  size_t m_width;
+  size_t m_height;
+
+  signal_t m_widthChanged;
+  signal_t m_heightChanged;
+
+  GraphicsRectangle *m_background;
 };
 
 #endif
--- super-ez-lib/widget/src/Widget.cpp
+++ super-ez-lib/widget/src/Widget.cpp
@@ -1,5 +1,33 @@
 #include "../include/Widget.hpp"
 
 Widget::Widget(GraphicsItem *parent)
-: GraphicsItem(parent)
-{}
+: GraphicsItem(parent),
+  m_width(0),
+  m_height(0),
+  m_background(nullptr)
+{
+  m_background = new GraphicsRectangle(this);
+
+  widthChanged([this]() {
+    m_background->setWidth(width());
+  });
+  heightChanged([this]() {
+    m_background->setHeight(height());
+  });
+}
+
+void Widget::setWidth(const size_t width)
+{
+  if (width != m_width) {
+    m_width = width;
+    m_widthChanged();
+  }
+}
+
+void Widget::setHeight(const size_t height)
+{
+  if (height != m_height) {
+    m_height = height;
+    m_heightChanged();
+  }
+}
