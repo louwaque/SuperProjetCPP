806ca5c2acfb9cf6963e998705cd00e9337614c0
Travis CI <travis@travis-ci.org>
2017-06-07 11:33:17 +0000

    Deploy code docs to GitHub Pages Travis build: 86
    Commit: f084868dbf29c1ec6849d57a2a1862e0caf27e43

--- html/group__widget.js
+++ html/group__widget.js
@@ -4,11 +4,11 @@ var group__widget =
       [ "Button", "class_button.html#a533e3a3815061a3cbf7ae2539af4b206", null ],
       [ "Button", "class_button.html#a2cfe02cc2981d9878f6c4660e5680388", null ],
       [ "clicked", "class_button.html#a3869de27faa1d62b47f83dd752ec43fc", null ],
-      [ "label", "class_button.html#a66449ddac81dd3023c604e1cd97fd617", null ],
-      [ "label", "class_button.html#ae87770ed8c4e71268195d0937a9fe33a", null ],
       [ "meHandleEvent", "class_button.html#ac8d84bc467ce5a7ac5791df121fe32dd", null ],
       [ "minimumHeight", "class_button.html#aec7d6551d9dd87879b985da6e3b34f3e", null ],
-      [ "minimumWidth", "class_button.html#a0139d451c5325175a69af8d4e4f0ee67", null ]
+      [ "minimumWidth", "class_button.html#a0139d451c5325175a69af8d4e4f0ee67", null ],
+      [ "setText", "class_button.html#ac6a233c876164a52d54ef7f1b3fb78d1", null ],
+      [ "text", "class_button.html#ad9fb8858260a4e8ace09e98fdf4db736", null ]
     ] ],
     [ "Label", "class_label.html", [
       [ "Label", "class_label.html#a9845a587fdead39627009758c789e914", null ],
