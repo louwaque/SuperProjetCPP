c527d22cce87d73b9410f99b4e6b4a75e528aed4
L0L022 <L0L022@openmailbox.org>
2017-06-12 00:02:26 +0200

    Ajoute Pixel

---
+++ super-ez-lib/core/include/Pixel.hpp
@@ -0,0 +1,49 @@
+#ifndef PIXEL_HPP
+#define PIXEL_HPP
+
+#include <ez-lib/ez-draw++.hpp>
+#include <memory>
+
+#include "Point.hpp"
+#include "Color.hpp"
+
+class Image;
+
+/*!
+ * \file Pixel.hpp
+ * \brief Classe Pixel
+ * \author Loïc Escales, Leo Sudreau
+ * \version 0.1
+ * \date 11 juin 2017
+ */
+
+ /*!
+  * \class Pixel
+  * \brief Représente un pixel
+  */
+
+class Pixel
+{
+public:
+  Color color() const;
+  void setColor(const Color &color);
+
+  uint8_t alpha() const;
+  void setAlpha(const uint8_t alpha);
+
+  Point position() const { return m_position; }
+
+private:
+  Pixel(std::shared_ptr<EZImage> image, const Point &position);
+
+  EZPixel pixel();
+  const EZPixel pixel() const;
+
+  friend Image;
+
+private:
+  std::shared_ptr<EZImage> m_image;
+  const Point m_position;
+};
+
+#endif
---
+++ super-ez-lib/core/src/Pixel.cpp
@@ -0,0 +1,43 @@
+#include "../include/Pixel.hpp"
+
+Pixel::Pixel(std::shared_ptr<EZImage> image, const Point &position)
+: m_image(image),
+  m_position(position)
+{}
+
+Color Pixel::color() const
+{
+  const EZPixel p = pixel();
+  return Color(uint8_t(p.getRed()), p.getGreen(), p.getBlue());
+}
+
+void Pixel::setColor(const Color &color)
+{
+  EZPixel p = pixel();
+  p.setRed(color.red());
+  p.setGreen(color.green());
+  p.setBlue(color.blue());
+}
+
+uint8_t Pixel::alpha() const
+{
+  return pixel().getAlpha();
+}
+
+void Pixel::setAlpha(const uint8_t alpha)
+{
+  pixel().setAlpha(alpha);
+}
+
+EZPixel Pixel::pixel()
+{
+  if (m_image)
+    return m_image->getPixel(m_position.x(), m_position.y());
+  else
+    throw std::runtime_error("Pixel::pixel image ptr is null");
+}
+
+const EZPixel Pixel::pixel() const
+{
+  return pixel();
+}
